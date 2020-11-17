4fca9e6a0c5d54e8309262d905492ff35161de9f
jaguste.pranav <jaguste.pranav31@gmail.com>
2020-11-17 20:37:28 +0530

    [pranav] Add. Check for employee presence

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,4 +1,7 @@
 console.log("welcome to employee wage computation problem");
+
+//UC1
+console.log("UC1");
 let checkForEmployeePresence= Math.floor((Math.random()*10)%2);
 const IS_ABSENT=0;
 if(checkForEmployeePresence==IS_ABSENT)
@@ -9,3 +12,4 @@ else
 {
     console.log("employee is present");
 }
+
